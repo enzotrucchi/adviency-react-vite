f416defe0742161d227697ceee9503b5662d8fb7
Enzo Trucchi <trucchienzo@gmail.com>
2022-12-02 13:34:25 -0300

    Add background img

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import { useState } from "react";
 
+//css
 import "./App.css";
 
 function App() {
-	const [regalos, setRegalos] = useState([
-		"Caja de bombones",
-		"Lapicera",
-		"Skate",
-	]);
+	const [regalos, setRegalos] = useState(["Camiseta", "Medias", "Calzones"]);
 
 	return (
 		<div className="App">
-			<h1>Regalos</h1>
+			<h1>Lista de Regalos</h1>
 
 			<ul>
-				{regalos.map((regalo) => (
-					<li>{regalo}</li>
-				))}
+				{regalos.map((regalo, index) => {
+					return <li key={index}>{regalo}</li>;
+				})}
 			</ul>
 		</div>
 	);
