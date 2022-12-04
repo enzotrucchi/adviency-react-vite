7a1d0a323e5ff49a1ab566607acba40cbdff6136
Enzo Trucchi <trucchienzo@gmail.com>
2022-12-04 20:23:27 -0300

    Add new gift funcionality

--- src/components/gifts/gifts.jsx
+++ src/components/gifts/gifts.jsx
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 import Gift from "./gift";
 
 import "./gifts.css";
+import NewGift from "./newGift";
 
 const Gifts = () => {
 	const [regalos, setRegalos] = useState(["Camiseta", "Medias", "Calzones"]);
 
+	function addGift(gift) {
+		setRegalos([...regalos, gift]);
+	}
+
 	return (
 		<div className="giftsList">
 			<h1>Lista de Regalos</h1>
+
+			<NewGift onCreate={addGift} />
 
 			<ul>
 				{regalos.map((regalo, index) => {
